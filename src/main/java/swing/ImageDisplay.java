e21fa1309ae05f8d0922a6aa480b9189aad46d89
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 12:55:16 +0000

    Update ImageDisplay.java

--- src/main/java/swing/ImageDisplay.java
+++ src/main/java/swing/ImageDisplay.java
@@ -1,2 +1,20 @@
-package swing;public interface ImageDisplay {
+package software.ulpgc.imageviewer;
+
+public interface ImageDisplay {
+    int width();
+    void clear();
+    void paint(String image, int offset);
+
+    void on(Dragged dragged);
+    void on(Released released);
+
+    interface Dragged {
+        Dragged Null = offset -> {};
+        void to(int offset);
+    }
+
+    interface Released {
+        Released Null = offset -> {};
+        void at(int offset);
+    }
 }
