6e9470c35bf383d055d71979dd9b749950c21875
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 12:57:32 +0000

    Update MainFrame.java

--- src/main/java/swing/MainFrame.java
+++ src/main/java/swing/MainFrame.java
@@ -1,2 +1,28 @@
-package swing;public class MainFrame {
+package software.ulpgc.imageviewer.swing;
+
+import software.ulpgc.imageviewer.ImageDisplay;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    private ImageDisplay imageDisplay;
+
+    public MainFrame(){
+        this.setTitle("Image Viewer");
+        this.setSize(800,600);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.add(createImageDisplay());
+    }
+
+    private Component createImageDisplay() {
+        SwingImageDisplay display = new SwingImageDisplay();
+        this.imageDisplay = display;
+        return display;
+    }
+
+    public ImageDisplay imageDisplay() {
+        return imageDisplay;
+    }
 }
