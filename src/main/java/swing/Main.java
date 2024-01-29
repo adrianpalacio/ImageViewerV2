290219da71710682594cea9d7884522603d4360e
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 12:57:00 +0000

    Update Main.java

--- src/main/java/swing/Main.java
+++ src/main/java/swing/Main.java
@@ -1,2 +1,16 @@
-package swing;public class Main {
+package software.ulpgc.imageviewer.swing;
+
+import software.ulpgc.imageviewer.Image;
+import software.ulpgc.imageviewer.ImageDisplay;
+import software.ulpgc.imageviewer.ImagePresenter;
+
+public class Main {
+    public static void main(String[] args) {
+        Image image = new MockImageLoader().load();
+        MainFrame mainFrame = new MainFrame();
+        ImageDisplay imageDisplay = mainFrame.imageDisplay();
+        new ImagePresenter(image, imageDisplay);
+        mainFrame.setVisible(true);
+
+    }
 }
