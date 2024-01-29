389c4d66d21f5e57069cade2815a96167b9fe3d5
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 12:59:45 +0000

    Update MockImageLoader.java

--- src/main/java/mocks/MockImageLoader.java
+++ src/main/java/mocks/MockImageLoader.java
@@ -1,2 +1,36 @@
-package mocks;public class MockImageLoader {
+package software.ulpgc.imageviewer.swing;
+
+import software.ulpgc.imageviewer.Image;
+import software.ulpgc.imageviewer.ImageLoader;
+
+public class MockImageLoader implements ImageLoader {
+    private final String[] images = new String[] {"RED", "GREEN", "BLUE"};
+    @Override
+    public Image load() {
+        return imageAt(0);
+    }
+
+    private Image imageAt(int i) {
+        return new Image() {
+            @Override
+            public String name() {
+                return images[i];
+            }
+
+            @Override
+            public Image prev() {
+                return imageAt((i + 1) % images.length);
+            }
+
+            @Override
+            public Image next() {
+                return imageAt(i > 0 ? i - 1 : images.length - 1);
+            }
+
+            @Override
+            public boolean equals(Object obj) {
+                return obj instanceof Image && ((Image) obj).name().equals(this.name());
+            }
+        };
+    }
 }
